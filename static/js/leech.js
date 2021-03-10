8b83d6dd73fce2ba97dc2476e498c95d33b787e1
river-tribe <rivertribe101@gmail.com>
2021-03-09 21:10:24 -0500

    new commit

--- static/js/leech.js
+++ static/js/leech.js
@@ -1,7 +1,24 @@
-function YesLeech() {
+const passes = {'Alison':'/*AlisonS*/', 'Kale':'/*KaleE*/', 'Antoni':'/*AntoniH*/'};
+const namearr = Object.keys(passes);
+
+function textChecked() {
   window.location.href = 'https://river-tribe.github.io/leech_pic.html';
 }
 
-function NoLeech() {
-  alert('INCORRECT. PICK AGAIN.');
+function textFailed() {
+  alert('Incorrect username or password. Try again.');
+}
+
+function checkText() {
+  var currentname = document.getElementById('username').value;
+  var currentpass = document.getElementById('password').value;
+  if (namearr.includes(currentname)) {
+    if (currentpass == passes[currentname]) {
+      textChecked();
+    } else {
+      textFailed();
+    }
+  } else {
+    textFailed();
+  }
 }
